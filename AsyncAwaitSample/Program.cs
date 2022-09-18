59585d8853df378db6349a08df8ecd9b9cd3cb93
Fábio Brandão <fabio@idntx.com.br>
2022-09-18 12:31:39 -0300

    Correção Namespace

--- AsyncAwaitSample/Program.cs
+++ AsyncAwaitSample/Program.cs
@@ -2,7 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SyncAwaitSample
+namespace AsyncAwaitSample
 {
     class Program
     {
--- AsyncAwaitSample/Properties/AssemblyInfo.cs
+++ AsyncAwaitSample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("SyncAwaitSample")]
+[assembly: AssemblyTitle("AsyncAwaitSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SyncAwaitSample")]
+[assembly: AssemblyProduct("AsyncAwaitSample")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
